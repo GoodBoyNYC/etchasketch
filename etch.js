447545db3697dc3073507559b3bc6a13db5c6663
GoodBoyNYC <5098496+GoodBoyNYC@users.noreply.github.com>
2024-07-10 17:51:20 -0400

    refactor: Update grid size and styling
    - Update the grid size from 16 to 4 in the `etch.js` file
    - Add a nested loop in the `createGrid` function to create a grid of div elements
    - Set the text content of each div element to the x-coordinate value
    - Add the "col" class to each div element
    - Update the CSS in the `style.css` file to adjust the flex properties and background color of the grid

--- etch.js
+++ etch.js
@@ -1,11 +1,22 @@
-let gridSize=16;
+let gridSize=4;
 let container = document.querySelector('.container');
 
 function createGrid(gridSize){
-    for(let i=0;i<gridSize;i++){
+    for(let x=0;x<gridSize;x++){
+        for(let y=0;y<gridSize;y++){
         let column = document.createElement("div");
-        column.textContent="Added";
+        column.textContent=x;
         container.appendChild(column);
+        column.classList.add("col");
+        //column.style.cssText="flex: 1 0 20%;margin: 5px;background-color: grey;color: orange;border: 1px black solid;padding: 5px;"
+        }
     }
 }
 createGrid(gridSize);
+
+// adds class "new" to your new div
+//div.classList.add("new");
+// removes "new" class from div
+//div.classList.remove("new");
+// if div doesn't have class "active" then add it, or if it does, then remove it
+//div.classList.toggle("active");
