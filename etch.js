363463962eb437d849bab35ce41828392bc955ff
GoodBoyNYC <5098496+GoodBoyNYC@users.noreply.github.com>
2024-07-10 23:37:40 -0400

    refactor: Update grid size and styling, and optimize grid creation
    fix createGrid to implement rows and cells. updated eventlistener. add reset button.

--- etch.js
+++ etch.js
@@ -1,16 +1,21 @@
-let gridSize = 16;
-let container = document.querySelector('.container');
+let gridSize = 20;
+const container = document.querySelector('.container');
+const btnReset = document.querySelector('.reset');
 let gridArray = [];
+
 function createGrid(gridSize) {
+    container.innerHTML = ''
     for (let x = 0; x < gridSize; x++) {
+        let row = document.createElement("div");
+        container.appendChild(row);
+        row.classList.add("row");
         for (let y = 0; y < gridSize; y++) {
-            let column = document.createElement("div");
-            //column.textContent = y;
-            container.appendChild(column);
-            column.classList.add("col");
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            row.appendChild(cell);
         }
     }
-    gridArray = document.getElementsByClassName("col");
+    gridArray = document.getElementsByClassName("cell");
     Array.from(gridArray).forEach((cell) => {
         cell.addEventListener("mouseover", event => {
             cell.style.backgroundColor = 'grey';
