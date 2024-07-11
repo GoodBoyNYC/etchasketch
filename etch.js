6dd26fcdfc0ad5b760d45e144a150b3bba308ab5
GoodBoyNYC <5098496+GoodBoyNYC@users.noreply.github.com>
2024-07-10 23:07:37 -0400

    chore: Update grid size and styling. Add event listeners.

--- etch.js
+++ etch.js
@@ -1,22 +1,20 @@
-let gridSize=4;
+let gridSize = 16;
 let container = document.querySelector('.container');
-
-function createGrid(gridSize){
-    for(let x=0;x<gridSize;x++){
-        for(let y=0;y<gridSize;y++){
-        let column = document.createElement("div");
-        column.textContent=x;
-        container.appendChild(column);
-        column.classList.add("col");
-        //column.style.cssText="flex: 1 0 20%;margin: 5px;background-color: grey;color: orange;border: 1px black solid;padding: 5px;"
+let gridArray = [];
+function createGrid(gridSize) {
+    for (let x = 0; x < gridSize; x++) {
+        for (let y = 0; y < gridSize; y++) {
+            let column = document.createElement("div");
+            //column.textContent = y;
+            container.appendChild(column);
+            column.classList.add("col");
         }
     }
+    gridArray = document.getElementsByClassName("col");
+    Array.from(gridArray).forEach((cell) => {
+        cell.addEventListener("mouseover", event => {
+            cell.style.backgroundColor = 'grey';
+        });
+    });
 }
 createGrid(gridSize);
-
-// adds class "new" to your new div
-//div.classList.add("new");
-// removes "new" class from div
-//div.classList.remove("new");
-// if div doesn't have class "active" then add it, or if it does, then remove it
-//div.classList.toggle("active");
